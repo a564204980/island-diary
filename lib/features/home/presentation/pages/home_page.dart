484900290bd5b33cdccdf2b2a78b3a7266507f58
a564204980 <1140928814@qq.com>
2026-05-02 03:42:39 +0800

    🎈 perf: 优化

--- lib/features/home/presentation/pages/home_page.dart
+++ lib/features/home/presentation/pages/home_page.dart
@@ -15,7 +15,6 @@ import 'package:island_diary/features/profile/presentation/pages/profile_page.da
 import 'package:island_diary/shared/widgets/barrage/mood_barrage_wall.dart';
 import 'package:island_diary/shared/widgets/sprite_dialogue.dart';
 import 'package:island_diary/features/record/domain/models/diary_entry.dart';
-import 'package:island_diary/features/record/presentation/pages/decoration_page.dart';
 import 'package:island_diary/features/statistics/presentation/pages/statistics_page.dart';
 
 
